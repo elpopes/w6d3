272e09b714b95b73815b97cacccbf1a144bc2b29
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2022-12-15 11:40:07 -0500

    switch 3

--- manny_and_lorenzo_rails/manny_lorenzo_rails/app/controllers/users_controller.rb
+++ manny_and_lorenzo_rails/manny_lorenzo_rails/app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         
         if @user.save
-               redirect_to user_url(@user)
+            redirect_to user_url(@user)
         else
             render json: @user.errors.full_messages, status: 422
         end
