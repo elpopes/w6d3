a4d4d23d5412dfd6e295f4a5afa09748c58819e1
lorenzo <lorenzotijerina@gmail.com>
2022-12-15 20:48:11 -0500

    artshare

--- manny_and_lorenzo_rails/manny_lorenzo_rails/app/models/user.rb
+++ manny_and_lorenzo_rails/manny_lorenzo_rails/app/models/user.rb
@@ -3,7 +3,8 @@
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  username   :string           not null
+#  name       :string           not null
+#  email      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
